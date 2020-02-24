fb3c11a91b6da4ca8c1d2e70bae7dfcf16a847a8
MaxFunProger <maxts2003@mail.ru>
2020-02-24 22:31:26 +0400

    Added new ball (sprite) and it can interact with borders (also new sprites). Changed class Ball. Not cleared old function yet.

--- cycle.py
+++ cycle.py
@@ -1,10 +1,14 @@
 import pygame
 
 pygame.init()
-size = 1020, 700
-screen = pygame.display.set_mode(size)
+size_1 = 1020, 700
+screen = pygame.display.set_mode(size_1)
+screen.fill([0, 0, 0])
 FPS = 60
 MOVE = 30
+all_sprites = pygame.sprite.Group()
+h_borders = pygame.sprite.Group()
+v_borders = pygame.sprite.Group()
 
 
 class Field(pygame.sprite.Sprite):
@@ -61,6 +65,8 @@ class Field(pygame.sprite.Sprite):
 
 
 left = 600
+top = 650
+size = (70, 10)
 
 
 class Plat(pygame.sprite.Sprite):
@@ -98,25 +104,35 @@ class Plat(pygame.sprite.Sprite):
                 self.mv = False
 
 
-class Ball(Plat):
-    def __init__(self):
-        super().__init__()
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__(all_sprites)
         self.left_b = 600
         self.top_b = 600
         self.rad = 10
         self.fly = False
-        self.prev_b = [left, self.top]
+        self.prev_b = [left, top]
         self.cords_b = [self.left_b, self.top_b, self.rad]
-        self.u_b = 10
+        self.u_b = 2
         self.direct = [-self.u_b, -self.u_b]
+        self.image = pygame.Surface((2 * self.rad, 2 * self.rad), pygame.SRCALPHA, 32)
+        pygame.draw.circle(self.image, pygame.Color('blue'), (self.rad, self.rad), self.rad)
+        self.rect = pygame.Rect(x, y, 2 * self.rad, 2 * self.rad)
+
+    def update(self):
+        self.rect = self.rect.move(self.direct[0], self.direct[1])
+        if pygame.sprite.spritecollideany(self, h_borders):
+            self.direct[1] = -self.direct[1]
+        if pygame.sprite.spritecollideany(self, v_borders):
+            self.direct[0] = -self.direct[0]
 
     def render_ball(self, scr):
         if not self.fly:
             pygame.draw.circle(scr, pygame.Color('black'), (self.prev_b[0],
                                                           self.prev_b[1]), self.cords_b[2], 0)
-            pygame.draw.circle(scr, pygame.Color('red'), (left + self.size[0] // 2,
-                                                          self.top - self.size[1]), self.cords_b[2], 0)
-            self.prev_b = [left + self.size[0] // 2, self.top - self.size[1]]
+            pygame.draw.circle(scr, pygame.Color('red'), (left + size[0] // 2,
+                                                          top - size[1]), self.cords_b[2], 0)
+            self.prev_b = [left + size[0] // 2, top - size[1]]
         else:
             pygame.draw.circle(scr, pygame.Color('black'), (self.prev_b[0], self.prev_b[1]), self.cords_b[2], 0)
             pygame.draw.circle(scr, pygame.Color('red'), (self.cords_b[0], self.cords_b[1]), self.cords_b[2], 0)
@@ -126,8 +142,8 @@ class Ball(Plat):
         if self.fly:
             pass
         else:
-            self.left_b = left + self.size[0] // 2
-            self.top_b = self.top - self.size[1] // 2
+            self.left_b = left + size[0] // 2
+            self.top_b = top - size[1] // 2
             self.fly = True
             self.cords_b = [self.left_b, self.top_b, self.rad]
             pygame.time.set_timer(MOVE, 20)
@@ -145,9 +161,26 @@ class Ball(Plat):
         self.cords_b[1] += self.direct[1]
 
 
+class Border(pygame.sprite.Sprite):
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(all_sprites)
+        if x1 == x2:
+            self.add(v_borders)
+            self.image = pygame.Surface([1, y2 - y1])
+            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+        else:
+            self.add(h_borders)
+            self.image = pygame.Surface([x2 - x1, 1])
+            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
+
+
 f = Field()
-b = Ball()
+b = Ball(600, 600)
 pl = Plat()
+Border(5, 5, size_1[0] - 5, 5)
+Border(5, size_1[1] - 5, size_1[0] - 5, size_1[1] - 5)
+Border(5, 5, 5, size_1[1] - 5)
+Border(size_1[0] - 5, 5, size_1[0] - 5, size_1[1] - 5)
 running = True
 screen.fill((0, 0, 0))
 f.render(screen)
@@ -179,5 +212,12 @@ while running:
     clock.tick(FPS)
     f.render(screen)
     pl.render(screen)
+    all_sprites.draw(screen)
+    all_sprites.update()
+    h_borders.draw(screen)
+    h_borders.update()
+    v_borders.draw(screen)
+    v_borders.update()
     b.render_ball(screen)
     pygame.display.flip()
+    screen.fill([255, 255, 255])
