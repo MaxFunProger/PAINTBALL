1fcbebaf3e38de41c05285aa63c055aece7cb5d2
MaxFunProger <maxts2003@mail.ru>
2020-02-28 22:57:15 +0400

    Updated class Plat globally. Now it is a sprite, not just an image. P.S. I still have only one none-sprite class Field.

--- cycle.py
+++ cycle.py
@@ -9,6 +9,7 @@ MOVE = 30
 all_sprites = pygame.sprite.Group()
 h_borders = pygame.sprite.Group()
 v_borders = pygame.sprite.Group()
+plate = pygame.sprite.Group()
 
 
 class Field(pygame.sprite.Sprite):
@@ -70,8 +71,8 @@ size = (70, 10)
 
 
 class Plat(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
+    def __init__(self, x, y):
+        super().__init__(all_sprites)
         self.size = (70, 10)
         self.prev = 600
         self.top = 650
@@ -80,28 +81,24 @@ class Plat(pygame.sprite.Sprite):
         self.mv = False
         self.u = 10
         self.cords = [left, self.top]
+        self.image = pygame.Surface([self.size[0], self.size[1]])
+        self.image.fill(pygame.Color('orange'))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.flag = False
+        plate.add(self)
 
-    def render(self, scr):
-        self.cords = [left, self.top]
-        pygame.draw.rect(scr, pygame.Color('black'), (self.prev, self.cords[1], self.size[0], self.size[1]), 0)
-        pygame.draw.rect(scr, pygame.Color('purple'), (self.cords[0], self.cords[1], self.size[0], self.size[1]), 0)
-
-    def pl_move(self, k):
-        global left
-        if k == 'l':
-            if left - self.u >= 0:
-                self.prev = left
-                left -= self.u
+    def update(self):
+        if pl.mv:
+            if self.dir == 'l':
+                if self.rect.x - self.u >= 0:
+                    self.rect = self.rect.move(-self.u, 0)
             else:
-                self.prev = left
-                self.mv = False
-        elif k == 'r':
-            if left + self.u + self.size[0] <= 1020:
-                self.prev = left
-                left += self.u
-            else:
-                self.prev = left
-                self.mv = False
+                if self.rect.x + self.u <= size_1[0] - self.size[0]:
+                    self.rect = self.rect.move(self.u, 0)
+        else:
+            pass
 
 
 class Ball(pygame.sprite.Sprite):
@@ -113,7 +110,7 @@ class Ball(pygame.sprite.Sprite):
         self.fly = False
         self.prev_b = [left, top]
         self.cords_b = [self.left_b, self.top_b, self.rad]
-        self.u_b = 2
+        self.u_b = 5
         self.direct = [-self.u_b, -self.u_b]
         self.image = pygame.Surface((2 * self.rad, 2 * self.rad), pygame.SRCALPHA, 32)
         pygame.draw.circle(self.image, pygame.Color('blue'), (self.rad, self.rad), self.rad)
@@ -125,40 +122,8 @@ class Ball(pygame.sprite.Sprite):
             self.direct[1] = -self.direct[1]
         if pygame.sprite.spritecollideany(self, v_borders):
             self.direct[0] = -self.direct[0]
-
-    def render_ball(self, scr):
-        if not self.fly:
-            pygame.draw.circle(scr, pygame.Color('black'), (self.prev_b[0],
-                                                          self.prev_b[1]), self.cords_b[2], 0)
-            pygame.draw.circle(scr, pygame.Color('red'), (left + size[0] // 2,
-                                                          top - size[1]), self.cords_b[2], 0)
-            self.prev_b = [left + size[0] // 2, top - size[1]]
-        else:
-            pygame.draw.circle(scr, pygame.Color('black'), (self.prev_b[0], self.prev_b[1]), self.cords_b[2], 0)
-            pygame.draw.circle(scr, pygame.Color('red'), (self.cords_b[0], self.cords_b[1]), self.cords_b[2], 0)
-            self.prev_b = self.cords_b.copy()
-
-    def release(self):
-        if self.fly:
-            pass
-        else:
-            self.left_b = left + size[0] // 2
-            self.top_b = top - size[1] // 2
-            self.fly = True
-            self.cords_b = [self.left_b, self.top_b, self.rad]
-            pygame.time.set_timer(MOVE, 20)
-
-    def mover(self):
-        if self.cords_b[0] - self.rad <= 2:
-            self.direct[0] = self.u_b
-        elif self.cords_b[0] + self.rad >= 1018:
-            self.direct[0] = -self.u_b
-        if self.cords_b[1] - self.rad <= 2:
-            self.direct[1] = self.u_b
-        elif self.cords_b[1] + self.rad >= 698:
-            self.direct[1] = -self.u_b
-        self.cords_b[0] += self.direct[0]
-        self.cords_b[1] += self.direct[1]
+        if pygame.sprite.spritecollideany(self, plate):
+            self.direct[1] = -self.direct[1]
 
 
 class Border(pygame.sprite.Sprite):
@@ -176,7 +141,7 @@ class Border(pygame.sprite.Sprite):
 
 f = Field()
 b = Ball(600, 600)
-pl = Plat()
+pl = Plat(500, 500)
 Border(5, 5, size_1[0] - 5, 5)
 Border(5, size_1[1] - 5, size_1[0] - 5, size_1[1] - 5)
 Border(5, 5, 5, size_1[1] - 5)
@@ -184,8 +149,6 @@ Border(size_1[0] - 5, 5, size_1[0] - 5, size_1[1] - 5)
 running = True
 screen.fill((0, 0, 0))
 f.render(screen)
-pl.render(screen)
-b.render_ball(screen)
 clock = pygame.time.Clock()
 while running:
     for event in pygame.event.get():
@@ -201,23 +164,17 @@ while running:
                 pl.dir = 'r'
                 pl.mv = True
             elif event.key == pygame.K_SPACE:
-                b.release()
+                pass
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 pl.mv = False
-        elif event.type == MOVE:
-            b.mover()
-    if pl.mv:
-        pl.pl_move(pl.dir)
     clock.tick(FPS)
     f.render(screen)
-    pl.render(screen)
     all_sprites.draw(screen)
     all_sprites.update()
     h_borders.draw(screen)
     h_borders.update()
     v_borders.draw(screen)
     v_borders.update()
-    b.render_ball(screen)
     pygame.display.flip()
     screen.fill([255, 255, 255])
